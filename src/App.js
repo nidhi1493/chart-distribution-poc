b73f2081d4a58cf34f2dd0b9a9a403dce659a453
nidhi1493 <nidhi.yadv@gmail.com>
2024-01-18 18:49:39 -0500

    - inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Main from "./components/Main";
+import "./App.css";
 
 function App() {
+  const [apiData, setApiData] = useState({ name: "manish" }); //here goes default object
+  useEffect(() => {
+    //Todo: here goes asyn call
+  }, [apiData]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main apiData={apiData} /> //pass api respose as props to child component
     </div>
   );
 }
